98bcab380e39350aca488f13953dc598b0dfdf1c
Swapnil Angarkhe <swapnilang21@gmail.com>
2026-01-16 12:01:46 +0000

    marquee: 'marquee 40s linear infinite', make it 30s

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -97,7 +97,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        marquee: 'marquee 40s linear infinite',
+        marquee: 'marquee 30s linear infinite',
       },
     },
   },
