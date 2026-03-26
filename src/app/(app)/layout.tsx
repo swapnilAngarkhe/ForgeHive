a6174b726e254bc44c9d15d781a7069e89245d9a
Swapnil Angarkhe <swapnilang21@gmail.com>
2026-03-26 19:59:09 +0000

    we are still polishing the tools page with this one specially the distan

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -14,7 +14,7 @@ export default async function AppLayout({
   return (
     <div className="flex min-h-screen w-full flex-col">
       <AppHeader user={user} />
-      <main className="flex flex-1 bg-muted/40 pt-24 justify-center">
+      <main className="flex flex-1 bg-muted/40 pt-16 justify-center">
         <div className="w-full max-w-5xl px-4">
           {children}
         </div>
